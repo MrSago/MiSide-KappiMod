003a6438fcbfbd3f660ec68cbc4844d43acc379c
MrSago <37180898+MrSago@users.noreply.github.com>
2025-05-05 04:09:17 +0500

    feat: add dialogue constants for Chibi Mita

--- ObjectNames.cs
+++ ObjectNames.cs
@@ -5,4 +5,6 @@ public static class ObjectNames
     public const string AIHASTO_INTRO_SCENE = "SceneAihasto";
     public const string MAIN_MENU_SCENE = "SceneMenu";
     public const string ENDING_GAME_SCENE = "Scene 16 - TheEnd";
+    public const string CHIBIMITA_SCENE = "Scene 9 - ChibiMita";
+    public const string CHIBIMITA_BROKEN_DIALOGUE = "3D TextFactory 5";
 }
