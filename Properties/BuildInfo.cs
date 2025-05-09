d03f62f74302fa6636ea98951a9d4200113019bb
MrSago <37180898+MrSago@users.noreply.github.com>
2025-05-10 01:12:36 +0500

    fix: update COMPANY constant to correct value for accurate build information

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -5,7 +5,7 @@ internal static class BuildInfo
     public const string NAME = "KappiMod";
     public const string DESCRIPTION = "Just for fun!";
     public const string AUTHOR = "MrS4g0";
-    public const string COMPANY = null;
+    public const string COMPANY = "KappiMod Team";
     public const string VERSION = "0.5.1";
     public const string GAMEVERSION = null;
     public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-KappiMod";
