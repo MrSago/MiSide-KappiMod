dc9e2dde49345db6a24cf3fca1a6840b12938dbd
MrSago <37180898+MrSago@users.noreply.github.com>
2025-06-04 01:17:23 +0500

    feat: add LOCATION3 constant for Scene 3 - WeTogether

--- Constants/SceneName.cs
+++ Constants/SceneName.cs
@@ -6,6 +6,7 @@ public static class SceneName
     public const string AIHASTO_INTRO = "SceneAihasto";
     public const string MAIN_MENU = "SceneMenu";
     public const string CHAPTER_TWO = "Scene 2 - InGame";
+    public const string LOCATION3 = "Scene 3 - WeTogether";
     public const string BACKROOMS = "Scene 7 - Backrooms";
     public const string CHIBIMITA = "Scene 9 - ChibiMita";
     public const string ENDING_GAME = "Scene 16 - TheEnd";
